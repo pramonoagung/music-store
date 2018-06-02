f2fb4bb9e210d4f37f128dac98c283ca18afe3a1
Agung Pramono <agung.prasatu@gmail.com>
2018-06-03 00:45:52 +0700

    Add custom view for the header component

--- src/components/header.js
+++ src/components/header.js
@@ -14,7 +14,16 @@ const Header = () => {
 
 const styles = {
   viewStyle: {
-    backgroundColor: 'lightblue'
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop:5,
+    shadowColor: 'black',
+    shadowOffset: { width:0, height: 2},
+    shadowOpacity: 0.2,
+    elevation:2,
+    position: 'relative'
   },
   textStyle: {
     fontSize: 20
