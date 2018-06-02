de698756fd5eabe850d8f36713caa900b01e6607
Agung Pramono <agung.prasatu@gmail.com>
2018-06-03 00:16:52 +0700

    Add view wrapper

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,25 @@
 // Import libs for making component
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 // Make a component
 const Header = () => {
-    return <Text>Album</Text>;
-}
+  const { textStyle, viewStyle } = styles;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Album</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: 'lightblue'
+  },
+  textStyle: {
+    fontSize: 20
+  }
+};
+
 // Make the component available to the other parts if the app
 export default Header;
