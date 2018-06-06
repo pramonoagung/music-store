b21e32eea795258c2d12ca6bf5a6dc12f1241f7e
Agung Pramono <agung.prasatu@gmail.com>
2018-06-06 17:50:04 +0700

    Show AlbumList Component to the view

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 // Import a library to help create a component
 import React from "react";
-import { AppRegistry } from "react-native";
+import { AppRegistry, View } from "react-native";
 import Header from "./src/components/Header";
+import AlbumList from "./src/components/AlbumList";
 
 // Create a component
-const myApp = () => <Header headerTitle={'Album'}/>;
-
+const myApp = () => (
+  <View>
+    <Header headerTitle={"Album"} />
+    <AlbumList />
+  </View>
+);
 export default myApp;
 // Render it to the device
 AppRegistry.registerComponent("albums", () => myApp);
