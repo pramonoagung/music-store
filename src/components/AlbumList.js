9fc3396a69711bd8f17359bf40a1a9ba86481c59
Agung Pramono <agung.prasatu@gmail.com>
2018-06-06 18:14:12 +0700

    Change AlbumList to a Class component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from "react";
+import { View, Text } from "react-native";
 
-const AlbumList = () => {
+class AlbumList extends Component {
+  render() {
     return (
-        <View>
-            <Text>Album List</Text>
-        </View>
+      <View>
+        <Text>Album List</Text>
+      </View>
     );
-};
+  }
+}
 
 export default AlbumList;
