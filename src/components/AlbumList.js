6fdf29871caecae08a20703a632f8df29c949488
Agung Pramono <agung.prasatu@gmail.com>
2018-06-09 10:53:18 +0700

    Add initial album state and its update state

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,10 +3,13 @@ import { View, Text } from "react-native";
 import axios from "axios";
 
 class AlbumList extends Component {
+  //Initial albums state
+  state = { albums: [] };
+
   componentWillMount() {
     axios
       .get("https://rallycoding.herokuapp.com/api/music_albums")
-      .then(response => console.log(response));
+      .then(response => this.setState({ albums: response.data })); // Update Albums state from the new data retrieved  
   }
   render() {
     return (
