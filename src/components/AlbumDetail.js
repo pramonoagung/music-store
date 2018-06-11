b3b67f7f518c00538327cc24379d351fd5b47e3e
Agung Pramono <agung.prasatu@gmail.com>
2018-06-11 22:07:19 +0700

    Add Image and styling view

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,13 +1,41 @@
 import React from "react";
-import { Text } from "react-native";
-import Card from './Card';
+import { Text, View, Image } from "react-native";
+import Card from "./Card";
+import CardSection from "./CardSection";
 
-const AlbumDetail = props => {
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image } = album;
+  const { thumbnailStyle, headerContentStyles } = styles;
+
+  console.log(thumbnail_image);
+
   return (
     <Card>
-      <Text>{props.album.title}</Text>
+      <CardSection>
+        <View>
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={headerContentStyles}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerContentStyles: {
+    flexDirection: "column",
+    justifyContent: "space-around"
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  }
 };
 
 export default AlbumDetail;
